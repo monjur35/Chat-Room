e9056985d7d27e88102ba424bf15ba4587ca9288
monjurCEL <monjur@celimited.com>
2023-07-25 12:21:55 +0600

    DI-app module

---
+++ app/src/main/java/com/example/chat/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.example.chat.di
+
+import com.example.chat.network.dataSource.ChatWebSocketServiceImpl
+import com.example.chat.network.dataSource.MessageServiceImpl
+import com.example.chat.network.services.ChatWebSocketService
+import com.example.chat.network.services.MessageService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.websocket.WebSockets
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideKtorClient():HttpClient{
+        return HttpClient(CIO){
+            install(Logging)
+            install(WebSockets)
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    fun providesChatWebSocketService(client: HttpClient):ChatWebSocketService{
+        return ChatWebSocketServiceImpl(client)
+    }
+
+     @Provides
+    @Singleton
+    fun providesChatService(client: HttpClient):MessageService{
+        return MessageServiceImpl(client)
+    }
+
+
+}
